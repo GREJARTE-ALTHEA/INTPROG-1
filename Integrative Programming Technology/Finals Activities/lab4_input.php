561fc4a8b2b9961a0b3d78c416e066973e4dfe22
GREJARTE-ALTHEA <grejartealtheakyle_bsit@plmun.edu.ph>
2026-09-10 01:50:17 +0800

    Create lab4_input.php to process JSON data
    Add PHP script to handle JSON input and display username and password.

---
+++ Integrative Programming Technology/Finals Activities/lab4_input.php
@@ -0,0 +1,9 @@
+<?php
+$raw_data = file_get_contents('php://input');
+$data = json_decode($raw_data, true);
+
+if ($data) {
+    echo "username= " . $data['username'] . "\n";
+    echo "password= " . $data['password'] . "\n";
+}
+?>
